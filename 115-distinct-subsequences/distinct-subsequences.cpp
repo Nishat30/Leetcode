698dd19b6ef06df6992f8a192ec28b44a26acc2a
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-07 04:05:29 +0530

    Time: 27 ms (72.78%) | Memory: 26.9 MB (72.99%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    const int prime = 1e9 + 7;
     int solve(int i,int j,string s, string t,vector<vector<int>>& dp){
         if(j==0) return 1 ;//t is exhausted which means we have matched the string
         if(i==0) return 0; //s hii khtam phir toh khela khatam
@@ -18,6 +19,23 @@ public:
         int m=t.length();
         //memorization
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1)); //1 based indexing
-        return solve(n,m,s,t,dp);
+        // return solve(n,m,s,t,dp);
+        //tabultaion
+        //base case 
+        for(int i=0;i<=n;i++){
+            dp[i][0]=1;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j]=(dp[i-1][j-1] + dp[i-1][j]) % prime;
+                }else{
+                    dp[i][j]=dp[i-1][j];}
+            }
+        }
+        return dp[n][m];
     }
 };
