c9a8d409f1e7f14f702d0aa6ed019beb2a58c794
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-08 03:41:26 +0530

    Time: 7 ms (15.38%) | Memory: 19.8 MB (20.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -31,5 +31,23 @@ public:
             }
         }
         return dp[0][1];
+
+        //space optmization
+        vector<int> prev(2,0),cur(2,0);
+        prev[0]=0;
+        prev[1]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                long profit=0;
+                if(buy){
+                    profit = max(-prices[i] + prev[0] , 0+ prev[1]);
+                }else{ 
+                    profit= max(prices[i] + prev[1] , 0+ prev[0]);
+                }
+                cur[buy]=profit;
+            }
+            prev=cur;
+        }
+        return prev[1];
     }
 };
