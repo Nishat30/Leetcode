a751c23200092ae0a4c4cc04802ea76f827aa305
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-09 03:40:24 +0530

    Time: 413 ms (35.34%) | Memory: 211.3 MB (46.77%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    long solve(int i,int buy,vector<int>& prices,int n,vector<vector<vector<int>>>& dp,int cap){
-        if(cap==0) return 0;
-        if(i==n){return 0;} //kuch nhi bacha aur profit k lie
-        if(dp[i][buy][cap]!=-1)return dp[i][buy][cap];
-        long profit=0;
-        if(buy){
-            profit = max(-prices[i]+ solve(i+1,0,prices,n,dp,cap), 0+ solve(i+1,1,prices,n,dp,cap));
-            //ya toh kharida ya phir mtt kharido
-            //same as pick not pick wala case
-        }else{ //sell wala case
-            profit= max(prices[i] + solve(i+1,1,prices,n,dp,cap-1), 0+ solve(i+1,0,prices,n,dp,cap));
-        }
-        return dp[i][buy][cap]=profit;
-    }
     int maxProfit(vector<int>& prices) {
-        int cap=2;
+        //tabulation code
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
-        return (int)solve(0,1,prices,n,dp,cap);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        //since we initialize the dp with zero we dont need to consider the base case anymore
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<2;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy){
+                        dp[i][buy][cap] = max(-prices[i]+ dp[i+1][0][cap], 0+ dp[i+1][1][cap]);
+                        //ya toh kharida ya phir mtt kharido
+                        //same as pick not pick wala case
+                    }else{ //sell wala case
+                        dp[i][buy][cap]= max(prices[i] + dp[i+1][1][cap-1], 0+ dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
