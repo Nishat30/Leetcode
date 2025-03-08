f6b6a6db55cb142d9be4de78d0dd106fd2e6ee40
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-09 05:14:10 +0530

    Time: 181 ms (49.58%) | Memory: 150.5 MB (48.13%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+//trans=0,1,2,3
+//where 0 and 2 implie buy & 1 and 3 implies sell
+    long solve(int idx, int trans,vector<int>& prices,int n,vector<vector<int>>& dp){
+        if(trans==4){return 0;}
+        if(idx==n){return 0;}
+        if(dp[idx][trans]!=-1){return dp[idx][trans];}
+        long profit=0;
+        if(trans%2==0){ //buy wala case
+           profit=max(-prices[idx]+solve(idx+1,trans+1,prices,n,dp), 0+solve(idx+1,trans,prices,n,dp));
+        }else{//sell wala case
+           profit=max(prices[idx]+solve(idx+1,trans+1,prices,n,dp), 0+solve(idx+1,trans,prices,n,dp));
+        }
+        return dp[idx][trans]=profit;
+    }
     int maxProfit(vector<int>& prices) {
         //tabulation code
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        //since we initialize the dp with zero we dont need to consider the base case anymore
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<2;buy++){
-                for(int cap=1;cap<=2;cap++){
-                    if(buy){
-                        dp[i][buy][cap] = max(-prices[i]+ dp[i+1][0][cap], 0+ dp[i+1][1][cap]);
-                        //ya toh kharida ya phir mtt kharido
-                        //same as pick not pick wala case
-                    }else{ //sell wala case
-                        dp[i][buy][cap]= max(prices[i] + dp[i+1][1][cap-1], 0+ dp[i+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][2];
+        vector<vector<int>> dp(n+1,vector<int>(5,-1));
+        return solve(0,0,prices,n,dp);
     }
 };
