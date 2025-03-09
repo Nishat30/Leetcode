ecabe6beb62aa8abfdb4e451f1778de4d71e2ea4
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-09 05:45:11 +0530

    Time: 2 ms (83.65%) | Memory: 14.4 MB (79.68%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(k+1,0)));
-        //since we initialize the dp with zero we dont need to consider the base case anymore
-        for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<2;buy++){
-                for(int cap=1;cap<=k;cap++){
-                    if(buy){
-                        dp[i][buy][cap] = max(-prices[i]+ dp[i+1][0][cap], 0+ dp[i+1][1][cap]);
-                        //ya toh kharida ya phir mtt kharido
-                        //same as pick not pick wala case
-                    }else{ //sell wala case
-                        dp[i][buy][cap]= max(prices[i] + dp[i+1][1][cap-1], 0+ dp[i+1][0][cap]);
-                    }
+        vector<int> next(2*k+1,0),curr(2*k+1,0);
+        for(int idx=n-1;idx>=0;idx--){
+            for(int trans=0;trans<2*k;trans++){
+                long profit=0;
+                if(trans%2==0){ //buy wala case
+                    profit=max(-prices[idx]+next[trans+1], 0+next[trans]);
+                }else{//sell wala case
+                    profit=max(prices[idx]+next[trans+1], 0+next[trans]);
                 }
+                curr[trans]=profit;
             }
+            next=curr;
         }
-        return dp[0][1][k];
+        return next[0];
     }
 };
