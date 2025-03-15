875d783743f1fb67286ac8ce0d150ac6959f8eae
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-03-16 00:25:40 +0530

    Time: 28 ms (40.75%) | Memory: 60.8 MB (94.16%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int minCapability(vector<int>& nums, int k) {
-        int minReward = 1, maxReward = *max_element(nums.begin(), nums.end()),
-        totalHouses = nums.size();
-
-        // Use binary search to find the minimum reward possible.
-        while (minReward < maxReward) {
-            int midReward = (minReward + maxReward) / 2;
-            int possibleThefts = 0;
-
-            for (int index = 0; index < totalHouses; ++index) {
-                if (nums[index] <= midReward) {
-                    possibleThefts += 1;
-                    index++;
+    int minCapability(vector<int>& A, int k) {
+        int left = 1, right = 1e9, n = A.size();
+        while (left < right) {
+            int mid = (left + right) / 2, take = 0;
+            for (int i = 0; i < n; ++i)
+                if (A[i] <= mid) {
+                    take += 1;
+                    i++;
                 }
-            }
-
-            if (possibleThefts >= k)
-                maxReward = midReward;
+            if (take >= k)
+                right = mid;
             else
-                minReward = midReward + 1;
+                left = mid + 1;
         }
-
-        return minReward;
+        return left;
     }
 };
