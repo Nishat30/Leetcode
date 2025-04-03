710d40d3684993c84567780af28e0f60626113a3
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-04-04 04:47:09 +0530

    Time: 2 ms (11.32%) | Memory: 8.1 MB (59.45%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string ans;
-        for(char c:s){
-            ans.push_back(c);
-        }
-        for(char c:s){
-            ans.push_back(c);
-        }
-        return s.length()==goal.length() && ans.find(goal) != std::string::npos;
+        return s.length()==goal.length() && (s+s).find(goal) != std::string::npos;
     }
 };
