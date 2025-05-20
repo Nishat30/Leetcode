73e0bf19742d46e670dd806d5c29432d418bbffa
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-05-20 12:34:03 +0530

    Time: 128 ms (35.40%) | Memory: 325.4 MB (29.92%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        vector<int> diff(n + 1, 0);
-
-        for (const auto& query : queries) {
-            int li = query[0], ri = query[1];
-            diff[li] -= 1;
-            if (ri + 1 < n) {
-                diff[ri + 1] += 1;
+        int n=nums.size();
+        vector<int> diff(n+1,0);
+        for(auto i:queries){
+            int l=i[0],r=i[1];
+            diff[l]-=1;
+            if(r+1<=n){
+                diff[r+1]+=1;
             }
         }
-
-        int currentDecrement = 0;
-        for (int i = 0; i < n; ++i) {
-            currentDecrement += diff[i];
+        int cnt=0;
+        for(int i=0;i<nums.size();++i){
+            cnt+=diff[i];
             if(nums[i]!=0){
-            nums[i] += currentDecrement;}
+                nums[i]+=cnt;
+            }           
         }
-
-        for (int num : nums) {
-            if (num > 0) {
-                return false;
-            }
+        for(int i:nums){
+            if(i>0)return false;
         }
-
         return true;
     }
 };
