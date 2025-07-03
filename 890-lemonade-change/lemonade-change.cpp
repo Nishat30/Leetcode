e0777636579126897bcd094021e8c6cafbae0ce0
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-04 03:10:16 +0530

    Time: 0 ms (100.00%) | Memory: 87.1 MB (72.73%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five_cnt=0;
+        int ten_cnt=0;
+        int twenty_cnt=0;
+        for(int i:bills){
+            if(i==5)five_cnt++;
+            else if(i==10){
+                if(five_cnt==0)return false;
+                else ten_cnt++,five_cnt--;
+            }else{
+                if(five_cnt==0)return false;
+                else if(ten_cnt>=1 && five_cnt>=1){
+                    ten_cnt--;
+                    five_cnt--;
+                }else if(five_cnt>=3){
+                    five_cnt-=3;
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
