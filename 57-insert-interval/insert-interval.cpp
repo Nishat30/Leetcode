3cca7cedbbecb6fe0d214fc687658da9eaac1f8a
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-11 03:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (45.23%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -4,22 +4,19 @@ public:
         vector<vector<int>> res;
         int n=intervals.size();
         int i=0;
-        //left
         while(i<n && intervals[i][1]<newInterval[0]){
             res.push_back(intervals[i]);
-            i=i+1;
+            i++;
         }
-        //middle overlap
         while(i<n && intervals[i][0]<=newInterval[1]){
-            newInterval[0]= min(newInterval[0],intervals[i][0]);
-            newInterval[1]= max(newInterval[1],intervals[i][1]);
-            i=i+1;
+            newInterval[0]=min(intervals[i][0],newInterval[0]);
+            newInterval[1]=max(intervals[i][1],newInterval[1]);
+            i++;
         }
         res.push_back(newInterval);
-        //right
         while(i<n){
-            res.push_back(intervals[i]);
-            i=i+1;
+           res.push_back(intervals[i]); 
+           i++;
         }
         return res;
     }
