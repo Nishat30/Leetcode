29abdab36f97f15a3c084f269d2c6f9d80e0a66b
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-13 03:33:45 +0530

    Time: 3 ms (27.73%) | Memory: 22.9 MB (45.93%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n =ratings.size();
+        vector<int> cnt(n,1); //cnt[1,1,1]
+        //left neighbour check
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                cnt[i]=cnt[i-1]+1; 
+            } 
+        }
+        //[1,1,2]
+        
+        //right neighbour check
+        //cnt[1,1,2]
+        for(int i=n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                cnt[i]=max(cnt[i],cnt[i+1]+1);
+            }
+        }
+        //cnt=[2,1,2]
+        return accumulate(cnt.begin(),cnt.end(),0);
+    }
+};
+
+
