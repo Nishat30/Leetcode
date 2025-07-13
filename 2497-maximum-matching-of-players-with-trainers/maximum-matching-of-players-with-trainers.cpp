58d2b3793870994017ac4ab72168d44de5869dcb
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-13 21:53:50 +0530

    Time: 61 ms (5.00%) | Memory: 84.5 MB (5.47%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-        sort(players.begin(), players.end()); // sort players ascending
-
-        // Min-heap for trainers (so smallest available trainer is on top)
+        sort(players.begin(), players.end()); 
         priority_queue<int, vector<int>, greater<int>> pq;
-        for (int t : trainers) {
+        for (int t:trainers) {
             pq.push(t);
         }
 
         int cnt = 0;
-        for (int i = 0; i < players.size(); ++i) {
-            // Pop trainers until we find one strong enough
-            while (!pq.empty() && pq.top() < players[i]) {
-                pq.pop();  // too weak
+        for (int i=0; i<players.size(); ++i) {
+            while(!pq.empty() && pq.top()<players[i]) {
+                pq.pop();
             }
             if (!pq.empty()) {
-                cnt++;     // match found
-                pq.pop();  // remove matched trainer
+                cnt++;    
+                pq.pop(); 
             }
         }
         return cnt;
