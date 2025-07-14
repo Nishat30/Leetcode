2c2a9defbd2996847d2bb7eed483400847d91b79
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-07-14 17:23:33 +0530

    Time: 1 ms (16.43%) | Memory: 18.8 MB (12.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -6,21 +6,29 @@ public:
         vector<vector<bool>> visited(n,vector<bool>(m,false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0)visited[i][j]=true;
+                if(matrix[i][j]==0){
+                    visited[i][j]=true;
+                }
             }
         }
+        //(0,3) (i,j)
+        //row- (0,0) (0,1) (0,2) (0,3) i
+        //col- (0,3) (1,3) (2,3) j
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(visited[i][j]!=false){
-                    for(int k=0;k<n;k++){
-                        matrix[k][j]=0;
-                    }
+                if(visited[i][j]==true){
+                    //row-zero
                     for(int k=0;k<m;k++){
                         matrix[i][k]=0;
+                    }
+                    //column-zero
+                    for(int k=0;k<n;k++){
+                        matrix[k][j]=0;
                     }
                 }
             }
         }
+
 
     }
 };
