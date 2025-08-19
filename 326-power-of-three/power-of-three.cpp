2b56c0a0572715e188d5997f2422aff895201678
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-08-20 00:56:33 +0530

    Time: 2 ms (67.23%) | Memory: 8.8 MB (77.81%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 according to constraints
-        return n > 0 && maxPowerOf3 % n == 0;
-    }
+        if (n == 1) return true;
+        if (n <= 0 || n % 3 != 0) return false;
+        return isPowerOfThree(n / 3);
+   }
 };
