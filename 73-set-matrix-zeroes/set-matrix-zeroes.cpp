4e29b491b5d3e793ad4becc33aa10ad4459c84ad
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-08-21 01:12:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (51.44%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size();
-        int col0 = 1;
-        // Step 1: Mark rows & cols
-        for (int i=0; i<n;i++) {
-            if (matrix[i][0] == 0) col0 = 0;
-            for (int j=1;j<m;j++) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        vector<int> row(n,0);
+        vector<int> col(m,0); 
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-        // Step 2: Fill from (1,1) → (n-1,m-1)
-        for (int i=1;i<n;i++) {
-            for (int j=1; j<m;j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        // Finally, mark all (i, j) as 0
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
-        }
-        // Step 3: Handle first row
-        if (matrix[0][0] == 0) {
-            for (int j=0;j<m;j++) matrix[0][j] = 0;
-        }
-        // Step 4: Handle first col
-        if (col0 == 0) {
-            for (int i=0;i<n;i++) matrix[i][0] = 0;
         }
     }
 };
