5ca7096150457c2b722d57ddcccc4776b97df678
Mjtbvs <159575377+Nishat30@users.noreply.github.com>
2025-08-25 22:36:21 +0530

    Time: 15 ms (44.01%) | Memory: 25.1 MB (46.38%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -3,28 +3,21 @@ public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
         vector<int> ans;
-        vector<vector<bool>> visited(n, vector<bool>(m, true));
+        ans.reserve(n * m);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(visited[i][j]){
-                    int start = ans.size();
-                    ans.push_back(mat[i][j]);
-                    visited[i][j] = false;
+        for (int d = 0; d < n + m - 1; d++) {
+            int r = (d < m ? 0 : d - m + 1);
+            int c = (d < m ? d : m - 1);
 
-                    int a = i+1, b = j-1;
-                    while(a<n && b>=0 && visited[a][b]){
-                        ans.push_back(mat[a][b]);
-                        visited[a][b] = false;
-                        a++; b--;
-                    }
-
-                    // reverse this diagonal only
-                    if((i+j) % 2 == 0){
-                        reverse(ans.begin() + start, ans.end());
-                    }
-                }
+            vector<int> temp;
+            while (r < n && c >= 0) {
+                temp.push_back(mat[r][c]);
+                r++;
+                c--;
             }
+
+            if (d % 2 == 0) reverse(temp.begin(), temp.end());
+            ans.insert(ans.end(), temp.begin(), temp.end());
         }
         return ans;
     }
