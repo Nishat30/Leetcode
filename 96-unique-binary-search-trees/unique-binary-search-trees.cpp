879c27c56ac441c6d87dbe2cabd7e2041478ba86
Nishat Khanam <159575377+Nishat30@users.noreply.github.com>
2025-10-20 22:15:15 +0530

    Time: 2168 ms (5.04%) | Memory: 7.7 MB (98.20%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n<=1){
+            return 1;
+        }
+        unsigned long int res=0;
+        for(int i=0;i<n;i++){
+            res+=numTrees(i)*numTrees(n-i-1);
+        }
+        return res;
+    }
+};
