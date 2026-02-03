b0cebe579abcdd26b4aafa1a165d9cd98767864c
Nishat Khanam <159575377+Nishat30@users.noreply.github.com>
2026-02-03 20:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (99.69%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    bool isTrionic(vector<int>& nums) 
-    {
-        int p=-1,q=-1;
-        for(int i=1;i<nums.size()-1;i++)
-            {
-                if(nums[i]==nums[i-1]) //strictly condition
-                    return false;
-                if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-                if(p == -1) //seen a maxima
-                    p = i;
-                else 
-                    return false;
-                if(nums[i]<nums[i-1] && nums[i]<nums[i+1])
-                if(q==-1) //seen a minima
-                    q = i;
-                else
-                    return false;
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if (nums[0] >= nums[1]) {
+            return false;
+        }
+        int count = 1;
+        for (int i = 2; i < n; i++) {
+            if (nums[i - 1] == nums[i]) {
+                return false;
             }
-        return p!=-1 && q!=-1 && p<q; //seen and maxima comes first
-        
+            if ((nums[i - 2] - nums[i - 1]) * (nums[i - 1] - nums[i]) < 0) {
+                count++;
+            }
+        }
+        return count == 3;
     }
 };
