2a90cf3aed81025b7798dc45aeb04b208b10ee43
Nishat Khanam <159575377+Nishat30@users.noreply.github.com>
2026-05-20 03:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 54.5 MB (58.81%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mp;
-        for(int i:nums1){
-            mp[i]++;
-        }
-        for(int i:nums2){
-            if(mp.find(i)!=mp.end()){
-                return i;
+        int n=nums1.size();
+        int m=nums2.size();
+        int i=0,j=0;
+        while(i<n&&j<m){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else{
+                j++;
             }
         }
         return -1;
